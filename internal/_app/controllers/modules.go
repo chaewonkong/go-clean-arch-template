6051a1ebf913a577821815358658271f317338c1
chaewonkong <chaewonkong@nexon.co.kr>
2022-10-05 17:25:55 +0900

    refactor: add controller

--- internal/_app/controllers/modules.go
+++ internal/_app/controllers/modules.go
@@ -2,4 +2,4 @@ package controllers
 
 import "go.uber.org/fx"
 
-var Modules = fx.Options(fx.Invoke(BindRoutes))
+var Modules = fx.Options(fx.Invoke(BindRoutes), fx.Provide(NewController))
--- internal/_app/controllers/routes.go
+++ internal/_app/controllers/routes.go
@@ -1,13 +1,9 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
-func BindRoutes(e *echo.Echo) {
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "ok")
-	})
+func BindRoutes(e *echo.Echo, c Controller) {
+	e.GET("/", c.GetHealthCheck)
 }
